dd1bf00d7f2b12b8f996eaaf42fc63ecf3c71d47
Prencious Coder <devansh.panchal03@gmail.com>
2022-02-19 23:16:24 +0530

    Update Tutorial1.py

--- Web Scraping Using Selenium/Tutorial1.py
+++ Web Scraping Using Selenium/Tutorial1.py
@@ -6,16 +6,23 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-
+#Path of the respective chrome driver file
 PATH="/Volumes/Dev T7/vscode/projects/Pythonvscode/Web scraping/selenium/chromedriver"
+#create an instance for webdriver
 driver=webdriver.Chrome(PATH)
+#link of website which is to be interacted with.
 driver.get("https://techwithtim.net")
+#prints title of the site
 print(driver.title)
-
-search=driver.find_element_by_name("s")
+#searches the element by name in the html code
+search=driver.find_element_by_name("search")
+#enter key in the search instance(interacts)
 search.send_keys("test")
+#enters the key
 search.send_keys(Keys.RETURN)
 
+#this code block is used to instruct the program to wait until the elements have loaded in the web drower. Not using this can lead to exceptions.
+#Full explanation can be found in selenium documentation.
 try:
     main = WebDriverWait(driver, 10).until(
         EC.presence_of_element_located((By.ID, "main"))
